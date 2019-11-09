391ff7b1292603a5e9a52a5b5d70cec0d60b741c
Edin Zecevic <zeka23edin@hotmail.com>
2019-11-09 11:58:43 +0100

    practice

---
+++ practice_for_quz1.R
@@ -0,0 +1,64 @@
+typeof(age)
+
+mode(age)
+
+mode(player)
+
+mode(b1)
+
+x <- c(1, 2, 3, 4, 5)
+
+x <- c(1, 2, 3, "four", "five")
+
+mode
+
+x <- c(1, 2, 3, TRUE , FALSE)
+
+as.character(x)
+
+as.logical(x)
+
+x <- c(2, 4, 6, 8)
+
+names(x) <- letters[1:4]
+names(x)
+
+x[1]
+
+x
+
+x[length(x)]
+
+x[2:3]
+
+x[c(4,2,3,1)]
+
+x[c(TRUE, FALSE, TRUE, FALSE)]
+
+x[x==2]
+
+x[x!=2]
+
+x["a"]
+
+x[rep("a", 5)]
+
+x[x>1]
+
+x[TRUE]
+
+x[FALSE]
+
+x[as.logical(c(0, 1, pi, -10))]
+
+as.logical(c(0, 1, pi, -10))
+
+z<-c(1, 2, 3) + c(3, 2, 1)
+z<-c(1, 2, 3) * c(3, 2, 1)
+z<-c(1, 2, 3) ^ c(3, 2, 1)
+
+w<-c(2, 1) + c(1, 2, 3)
+
+w<-c(1, 2, 3, 4) + c
+
+w<- x+3
